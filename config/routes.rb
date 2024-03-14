f44d2ea9979bdd84785bf3a5d546fc3b3c73ceee
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-14 04:38:25 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,84 @@
+Rails.application.routes.draw do
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/create'
+    get 'addresses/update'
+    get 'addresses/destroy'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/thanks'
+    get 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'cart_items/index'
+    get 'cart_items/update'
+    get 'cart_items/destroy'
+    get 'cart_items/destroy_all'
+    get 'cart_items/create'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/unsubscribe'
+    get 'customers/withdraw'
+  end
+  namespace :public do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+  namespace :public do
+    get 'registrations/new'
+    get 'registrations/create'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :admin do
+    get 'order_details/update'
+  end
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/create'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
+  namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
+  namespace :admin do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
