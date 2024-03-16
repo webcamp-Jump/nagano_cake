f753b9adc1ba96e12bc45c5604e65aa2436e0123
“YuriShozen” <155799910+w2yuri@users.noreply.github.com>
2024-03-16 06:20:13 +0000

    途中経過、更新

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,5 +3,13 @@ class Public::ItemsController < ApplicationController
   end
 
   def show
+    @item = Item.find(params[:id])
+    @cart_item = CartItem
+  end
+  
+  private
+  
+  def item_params
+    params.require(:items).permit(:genre_id,:name,:introduction,:image,:pric,:is_active)
   end
 end
