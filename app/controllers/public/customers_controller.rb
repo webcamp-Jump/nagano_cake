fabc85e686bc2b2004e83b504e003cdc52819791
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-17 07:46:08 +0000

    ヘッダー編集レイアウトまだ_20240317 #22

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -14,8 +14,10 @@ before_action :authenticate_customer!
 
   def withdraw
   end
-  
+
+  private
+
   def customer_params
-  params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :password, :password_confirmation)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :password, :password_confirmation)
   end
 end
