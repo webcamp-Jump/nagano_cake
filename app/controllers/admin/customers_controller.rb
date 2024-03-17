ecd24f3f387a4a0862ea2483c5468f510f33eecf
EC2 Default User <156569903+Karen136i@users.noreply.github.com>
2024-03-17 14:08:37 +0000

    会員一覧ページの実装 #36

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -11,8 +11,20 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customers_path(@customer)
+  end
+  
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :telephone_number, :postal_code, :address)
   end
 end
+
