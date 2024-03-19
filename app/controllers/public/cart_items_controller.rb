fb16b4877f4729d59f7bdddf1e0e588d869b654c
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-19 17:42:07 +0000

    レイアウトとか_20240320 #35

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -10,18 +10,28 @@ class Public::CartItemsController < ApplicationController
     @cart_item = CartItem.new
   end
 
-  def create
+def create
+  existing_cart_item = current_customer.cart_items.find_by(item_id: cart_item_params[:item_id])
+
+  if existing_cart_item
+    existing_cart_item.amount += cart_item_params[:amount].to_i
+    if existing_cart_item.save
+      redirect_to public_cart_items_path, notice: 'カート内の商品数量を更新しました。'
+    else
+      logger.error existing_cart_item.errors.full_messages.join(', ')
+      redirect_to root_path, alert: 'カートの商品数量を更新できませんでした。'
+    end
+  else
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     if @cart_item.save
-      # 更新されたカートアイテムリストを取得
-      @cart_items = current_customer.cart_items
       redirect_to public_cart_items_path, notice: '商品をカートに追加しました。'
     else
       logger.error @cart_item.errors.full_messages.join(', ')
       redirect_to root_path, alert: '商品をカートに追加できませんでした。'
     end
   end
+end
 
   def update
     @cart_item = CartItem.find(params[:id])
