e189befa9c5c6d50450ea2a80268c1eb785d839f
makoto-niko <mmana19970613@gmail.com>
2024-03-21 08:20:24 +0000

    order関連

--- config/routes.rb
+++ config/routes.rb
@@ -23,13 +23,12 @@ Rails.application.routes.draw do
 
   post '/customers/sign_in', to: 'public/sessions#create'
 
-  namespace :public do
-    resources :addresses, only: [:index, :create, :update, :destroy]
-    resources :orders, only: [:new, :index, :show, :create] do
-        post 'confirm', on: :collection
-        get 'thanks', on: :collection
-    end
-
+ namespace :public, path: '' do
+  resources :addresses, only: [:index, :create, :update, :destroy]
+  resources :orders, only: [:new, :create, :index, :show] do
+    post 'confirm', on: :collection
+    get 'thanks', on: :collection
+  end
     resources :cart_items, only: [:index, :update, :destroy, :create, :show] do
       delete :destroy_all, on: :collection
     end
@@ -44,7 +43,7 @@ Rails.application.routes.draw do
   resources :cart_items, only: [:index, :update, :destroy, :create] do
     delete :destroy_all, on: :collection
   end
-
+  
   namespace :admin do
     resources :order_details, only: [:update]
     resources :orders, only: [:index, :show, :update]
@@ -55,3 +54,4 @@ Rails.application.routes.draw do
     resources :homes, only: [:top]
   end
 end
+
