79458af8c95bc420f57b43d7d3ef2ee1c7bedcde
EC2 Default User <156569903+Karen136i@users.noreply.github.com>
2024-03-21 10:24:02 +0000

    管理者ログインとアバウト

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,19 @@ Rails.application.routes.draw do
   # 退会処理
   get '/customers/unsubscribe', to: 'public/customers#unsubscribe', as: 'public_customers_unsubscribe'
   patch '/customers/withdraw', to: 'public/customers#withdraw'
-
+  
   get '/admin', to: 'admin/homes#top', as: 'admin_root'
   get 'customers/my_page', to: 'public/customers#show', as: 'public_customers'
   get 'customers/information/edit', to: 'public/customers#edit', as: 'public_customers_information_edit'
-
+  
   get 'addresses', to: 'public/addresses#index', as: 'addresses'
   get 'addresses/:id/edit', to: 'public/addresses#edit', as: 'addresses_edit'
   patch 'public/addresses/:id', to: 'public/addresses#update'
-
+  
+  # ここから(岩永)
+  get 'items', to: 'public/items#index', as: 'items'
+  get 'items/:id', to: 'public/items#show', as: 'item'
+  # ここまでの追加(岩永)
 
   devise_for :admin, controllers: {
     sessions: "admin/sessions"
@@ -24,16 +28,16 @@ Rails.application.routes.draw do
 
   post '/customers/sign_in', to: 'public/sessions#create'
 
- namespace :public, path: '' do
-  resources :addresses, only: [:index, :create, :update, :destroy]
-  resources :orders, only: [:new, :create, :index, :show] do
-    post 'confirm', on: :collection
-    get 'thanks', on: :collection
-  end
-    resources :cart_items, only: [:index, :update, :destroy, :create, :show] do
+  namespace :public do
+    resources :addresses, only: [:index, :create, :update, :destroy]
+    resources :orders, only: [:new, :index, :show, :create] do
+        post 'confirm', on: :collection
+        get 'thanks', on: :collection
+    end
+
+    resources :cart_items, only: [ :update, :destroy, :create, :show] do
       delete :destroy_all, on: :collection
     end
-    resources :items, except: [:destroy]
     resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
     resources :sessions, only: [:new, :create, :destroy]
     resources :registrations, only: [:new, :create]
@@ -55,4 +59,3 @@ Rails.application.routes.draw do
     resources :homes, only: [:top]
   end
 end
-
