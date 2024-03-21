b2a984f06bc6f5acf0bc9bd964324bc7fa7a6fc6
EC2 Default User <156569903+Karen136i@users.noreply.github.com>
2024-03-21 08:25:26 +0000

    修正1

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,19 +6,30 @@ class Public::CartItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:item_id])
+    @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
 
 def create
-  @cart_item = CartItem.new(cart_item_params)
-  @cart_item.customer_id = current_customer.id
+  existing_cart_item = current_customer.cart_items.find_by(item_id: cart_item_params[:item_id])
 
-  if @cart_item.save
-    redirect_to public_cart_items_path, notice: '商品をカートに追加しました。'
+  if existing_cart_item
+    existing_cart_item.amount += cart_item_params[:amount].to_i
+    if existing_cart_item.save
+      redirect_to cart_items_path, notice: 'カート内の商品数量を更新しました。'
+    else
+      logger.error existing_cart_item.errors.full_messages.join(', ')
+      redirect_to root_path, alert: 'カートの商品数量を更新できませんでした。'
+    end
   else
-    logger.error @cart_item.errors.full_messages.join(', ')
-    redirect_to root_path, alert: '商品をカートに追加できませんでした。'
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if @cart_item.save
+      redirect_to public_cart_items_path, notice: '商品をカートに追加しました。'
+    else
+      logger.error @cart_item.errors.full_messages.join(', ')
+      redirect_to root_path, alert: '商品をカートに追加できませんでした。'
+    end
   end
 end
 
