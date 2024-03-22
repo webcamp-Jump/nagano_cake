81b0121940043bbc42f7a82c9f1b9b6e5f6ae648
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-22 04:11:55 +0000

    注文情報更新_20240322 #58

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,5 +1,19 @@
 class Admin::OrderDetailsController < ApplicationController
-before_action :authenticate_admin!
+  before_action :authenticate_admin!
+
   def update
+    @order_detail = OrderDetail.find(params[:id])
+    if @order_detail.update(order_detail_params)
+      flash[:success] = "注文明細を更新しました"
+      redirect_to admin_order_path(@order_detail.order)
+    else
+      render 'admin/orders/show'
+    end
+  end
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
   end
 end
