4e3b9b9f5754935d79b4eb2bf62e110870c95207
EC2 Default User <156569903+Karen136i@users.noreply.github.com>
2024-03-22 10:47:17 +0000

    修正箇所一覧 #78

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,12 +3,12 @@
 class Public::RegistrationsController < Devise::RegistrationsController
  before_action :configure_sign_up_params, only: [:create,:update]
  before_action :authenticate_customer!
- 
- 
+
+
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :telephone_number, :postal_code, :address])
   end
-  
+
   # def create
   # # 新規登録処理
   # end
