abdb20d7c75f3fad5e2ebf9958b6bc1cb7d930bc
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-23 06:28:28 +0000

    バリケーション修正_20240323 #102

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -16,15 +16,17 @@ before_action :authenticate_admin!
     @order = Order.find(params[:id])
     @order.update(order_params)
     @order_details = @order.order_details
-  
+
     if @order.status == "入金確認"
       @order_details.each do |order_detail|
         order_detail.making_status = "製作待ち"
         order_detail.save
       end
       render :show
+    else
+      render :show
     end
-  # リダイレクトを削除して、そのままのページに留まる
+    
   end
 #ただし、ページをリロードするとフォームが再送信される可能性があるため、
 #更新処理が再度実行されてしまいます。
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   has_one_attached :image
-  belongs_to :genre
+  belongs_to :genre, optional: true
   has_many :cart_items
-  
+
+  validates :genre, presence: { message: "を選択してください" }
   validates :price, presence: true
   validates :name, presence: true, length: { maximum: 20 }
   validates :image, presence: true
