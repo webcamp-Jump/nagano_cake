ecd0a9f34ef38cafa3f01b1d901f5e38322f39b6
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-24 10:50:05 +0000

    ジャンル検索_20240324 #118

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,12 @@
 class Public::ItemsController < ApplicationController
 
   def index
-    @items = Item.where(is_active: true).search(params[:q]).page(params[:page]).per(12)
+    if params[:genre_id].present?
+      @genre = Genre.find_by(id: params[:genre_id])
+      @items = Item.where(genre_id: params[:genre_id], is_active: true).page(params[:page]).per(12)
+    else
+      @items = Item.where(is_active: true).search(params[:q]).page(params[:page]).per(12)
+    end
     @total_items = @items.total_count
   end
 
